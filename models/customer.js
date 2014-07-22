86a95a02cc2d0219089313917789d32c5682d539
yanxiaonan1988 <yanxiaonan1988@gmail.com>
2014-07-22 22:32:12 +0800

    add customer attribute
    add more common attributes for customer schema, because of using
    MongoDB, customer schema can be easily extended without database
    concern now, and later also.

--- models/customer.js
+++ models/customer.js
@@ -2,14 +2,57 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var customerSchema = new Schema({
-	cusId: String,
-	cusName: String,
-	cusType: Number,
+	cusId: String, //customer id
+	cusName: String, //customer name
+	cusType: Number, //customer type, individual or organization
 	sex: String,
 	birthday: Date,
-	papers: [{paperType: String, paperNo: String, startDate: Date, endDate: Date}],
+	papers: [{paperType: String, paperNo: String, authCntry: String, authOrg: String, startDate: Date, endDate: Date}],
 	contacts: [{contactType: String, contactValue: String}],
 	addresses: [{addressType: String, addressValue: String, postCode: String}],
+	tags: [tagName: String],
+	moneyLaundering: String, //code
+	salesStat: String, //code table
+	salesStatDate: Date,
+	individualInfo: {
+		employer: String,
+		maritalStat: String, //code table
+		healthyStat: String, //code table
+		industry: String, //code table
+		eduLvl: String, //code table
+		duty: String, //职务描述
+		prfssnLvl: String, //职级描述
+		techTitile: String, //职称描述
+		prof: String, //code table, 职业
+		individualStat: String, //code table, 个人状态
+		cntryOfBirth: String, //code
+		proOfBirth: String, //code
+		cityOfBirth: String, //code
+		empStat: String, //code
+		empStatDate: Date,
+		ethnicGroup: String, //code
+		preferLang: String, //code
+		nationality: String, //code
+		secondNat: String, //code
+	},
+	organizationInfo: {
+		contact: String,
+		contactPhone: String,
+		typeOfOrg: String, //code table
+		typeOfBussiness: String, //code table
+		regDate: Date,
+		legalStruct: String, //code table
+		statOrg: String, //code table
+		statDate: Date,
+		orgStartDate: Date,
+		orgEndDate: Date,
+		numOfEmp: Number,
+		regAddress: String,
+		regFund: Number,
+		corpRepres: String,
+		bizScope: String,
+		bizTerm:
+	},
 	createdDate: Date,
 	updatedDate: Date
 });
